d4ea2e8ad650b2a7c0e3d8d2c93bfe7e0bcd5b5f
JoseIstacuy <jistacuy-2026001@kinal.edu.gt>
2026-09-19 21:03:08 -0600

    Corrijo dashboard y variables de entorno

--- src/main/java/org/renacer/clinica/renacer/config/Credentials.java
+++ src/main/java/org/renacer/clinica/renacer/config/Credentials.java
@@ -10,8 +10,9 @@ package main.java.org.renacer.clinica.renacer.config;
  */
 public class Credentials{
     
-    public static final String URL_DB = System.getenv("URL_MYSQL_DB")+"/clinica_renacer_in4bm";
-    public static final String USER_DB = System.getenv("USER_MYSQL_DB");
+    public static final String URL_DB = System.getenv("URL_DB") + "/clinica_renacer_in4bm";
+    public static final String USER_DB = System.getenv("USER_DB");
     public static final String PASS_DB = System.getenv("PASS_DB");
+    
 }
 
